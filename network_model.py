7840a1f0040e8794ffde4b1a9d794cbead55b937
Rob Hanusa <rkhanusa@gmail.com>
2023-11-27 07:29:32 +0100

    Tried adjusting some constraints, more work needed

--- network_model.py
+++ network_model.py
@@ -10,6 +10,8 @@ import pandas as pd
 import gurobipy as gp
 from gurobipy import GRB
 
+import plots as pl
+
 # ---- Global variables ----
 M = 1000
 
@@ -39,7 +41,8 @@ C = (60, 120, 130, 50)
 # ---- Sets ---- 
 Nodes = range(16)  # Set of customers
 Satellites = range(4)  # Set of possible satellites
-Satellites_ind = (1, 3, 7, 12) # Set of possible satellites, adjusted for index
+Satellites_ind = (1, 3, 7, 12)  # Set of possible satellites, adjusted for index
+Not_satellites = [i for i in Nodes if i not in Satellites_ind]
 Bikes = range(6)
 
 model = gp.Model("Design") # Make Gurobi model
@@ -78,7 +81,7 @@ model.addConstr(gp.quicksum(a[k] for k in Satellites) <=3)
 
 # Bike cannot be assigned to a satellite if the satellite is not chosen
 for k in Satellites:
-    model.addConstr	(gp.quicksum(w[b,k] for b in Bikes) <= M*a[k])
+    model.addConstr(gp.quicksum(w[b,k] for b in Bikes) <= M*a[k])
 
 # Bike exits all nodes that it enters
 for i in Nodes:
@@ -86,10 +89,22 @@ for i in Nodes:
         model.addConstr(gp.quicksum(x[i,j,b] for j in Nodes if i != j)
                         == gp.quicksum(x[j,i,b] for j in Nodes if i != j))
         
-# Every node is entered at least once
-for j in Nodes:
-    model.addConstr(gp.quicksum(x[i,j,b] for i in Nodes for b in Bikes if i != j)
-                    >= 1)
+# # Every node is entered at least once
+# for j in Nodes:
+#     model.addConstr(gp.quicksum(x[i,j,b] for i in Nodes for b in Bikes if i != j)
+#                     >= 1)
+
+# Every non-satellite node is entered once
+for j in Not_satellites:
+    model.addConstr(gp.quicksum(x[i,j,b] for i in Nodes for b in Bikes 
+                                if (i != j and i not in Satellites_ind))
+                    == 1)
+    
+# # Every non-satellite node is exited once-----------
+# for i in Not_satellites:
+#     model.addConstr(gp.quicksum(x[i,j,b] for j in Nodes for b in Bikes 
+#                                 if (i != j and j not in Satellites_ind))
+#                     == 1)
         
 # Every bike leaves a satellite. If a satellite isn't chosen, bike may leave 
 # more than 1 satellite
@@ -110,6 +125,15 @@ for b in Bikes:
                                 if i != j) 
                     <= gp.quicksum(M*w[b,k] for k in Satellites))
     
+
+# Subtour elimination constraints
+num_customers = 16
+num_vehicles = 6
+u = model.addVars(num_customers, vtype=GRB.CONTINUOUS, lb=1, ub=num_customers-1, name="u")
+#model.addConstrs(u[i] - u[j] + num_customers * x[i, j, k] <= num_customers - 1 for i in range(num_customers) for j in range(1, num_customers) for k in range(num_vehicles) if i != j)
+model.addConstrs(u[i] - u[j] + num_customers * x[i, j, k] <= num_customers - 2 for i in range(1, num_customers) for j in range(1, num_customers) for k in range(num_vehicles) if i != j)
+# model.addConstrs(u[i] - u[j] + num_customers * x[i, j, k] <= num_customers - 2 for i in range(1, num_customers) for j in range(1, num_customers) for k in range(num_vehicles) if i != j)
+# model.addConstrs((gp.quicksum(x[i, j, k] for j in range(num_customers) if i != j) == 2) for k in range(num_vehicles) for i in Satellites_ind)
 
 # ---- Set objective
 
@@ -134,6 +158,8 @@ model.setObjective(obj, GRB.MINIMIZE)
 model.optimize()
 #%%
 
+import plots as pl
+
 x_values = np.zeros((len(Nodes), len(Nodes), len(Bikes)))
 
 # Fill in the values from the flattened array
@@ -143,3 +169,6 @@ for i in Nodes:
             if i != j:
                 x_values[i, j, b] = x[i, j, b].X
                 
+pl.plot_routes(x_values)
+
+np.save('x_values2.npy', x_values)
--- plots.py
+++ plots.py
@@ -8,83 +8,12 @@ Created on Thu Nov 23 13:29:03 2023
 import numpy as np
 import matplotlib.pyplot as plt
 
-from network_model import x_values
+#from network_model import x_values
 
-coordinates = np.genfromtxt('coords.csv', delimiter=',')
+# coordinates = np.genfromtxt('coords.csv', delimiter=',')          
 
-# routes = []
+x_values = np.load('x_values2.npy')
 
-# for bike in range(len(x_values[0,0,:])):
-#     route = []
-#     for i in range(len(x_values[:,0,0])):
-#         for j in range(len(x_values[0,:,0])):
-#             if x_values[i, j, bike] < 1:
-#                 pass
-#             else:
-#                 j = i-1
-#                 route.append(coordinates[i])
-#     routes.append(route)
-            
-
-routes = []
-
-# for bike in range(len(x_values[0,0,:])):
-#     route = []
-#     for i in range(len(x_values[:,0,0])):
-#         for j in range(len(x_values[0,:,0])):
-#             if x_values[i, j, bike] < 1:
-#                 print('i: ', i, ', j: ', j)
-#                 pass
-#             else:
-#                 print(f'bike: {bike}, j: {j}, i: {i}')
-#                 route.append(coordinates[j])
-#                 i = j 
-#                 j = 0
-#                 pass
-#     routes.append(route)
-    
-
-
-
-node_routes = []
-
-for bike in range(len(x_values[0,0,:])):
-    node_list = []
-    i = 0
-    flag = True
-    while i < len(x_values[:,0,0])-1 and i not in node_list:
-        
-        for j in range(len(x_values[0,:,0])):
-            print(f'True, i={i}, j={j}')
-            if x_values[i,j,bike] == 1:
-                flag = False
-                print(f'True, i={i}, j={j}')
-                node_list.append(i)
-                i=j
-                break
-            else:
-                flag = True
-                pass
-            
-        if flag:
-            i += 1
-            flag = False
-    
-    node_routes.append(node_list)
-
-
-
-route = []
-for i in node_list:
-    route.append(coordinates[i])
-    
-# Make route circular by adding back first element to end of list.
-route.append(route[0])
-
-    
-            
-    
-    
 bike1 = x_values[:,:,0]
 bike2 = x_values[:,:,1]
 bike3 = x_values[:,:,2]
@@ -92,8 +21,75 @@ bike4 = x_values[:,:,3]
 bike5 = x_values[:,:,4]
 bike6 = x_values[:,:,5]
 
+def make_node_routes(x_values):
+    node_routes = []
+    
+    for bike in range(len(x_values[0,0,:])):
+        node_list = []
+        i = 0
+        flag = True
+        while i < len(x_values[:,0,0]) and i not in node_list:
+            for j in range(len(x_values[0,:,0])):
+                if x_values[i,j,bike] == 1:
+                    flag = False
+                    node_list.append(i)
+                    i=j
+                    break
+                else:
+                    flag = True
+                    pass
+                
+            if flag:
+                i += 1
+                flag = False
+        
+        node_routes.append(node_list)
+        
+    return node_routes
 
-# Plot the coordinates
-plt.scatter(coordinates[:, 1], coordinates[:, 0])
-plt.title('Pharmacies')
-plt.show()
+node_routes = make_node_routes(x_values)
+
+
+
+def make_coordinate_routes(node_routes, coordinates):
+    routes = []
+    
+    for node_list in node_routes:
+        route = []
+        for i in node_list:
+            route.append(coordinates[i])
+        
+        # Make route circular by adding back first element to end of list.
+        route.append(route[0])
+        
+        routes.append(route)
+        
+    return routes
+
+
+#%%
+
+def plot_routes(x_values):
+    
+    coordinates = np.genfromtxt('coords.csv', delimiter=',')
+    
+    node_routes = make_node_routes(x_values)
+    
+    routes = make_coordinate_routes(node_routes, coordinates)
+    
+    fig, ax = plt.subplots()
+    
+    for route in routes:
+        x = [route[i][1] for i in range(len(route))]
+        y = [route[i][0] for i in range(len(route))]
+        plt.plot(x,y)
+    
+    # Plot the coordinates
+    ax.scatter(coordinates[:, 1], coordinates[:, 0])
+    for i, coord in enumerate(coordinates):
+        ax.annotate(str(i), (coord[1], coord[0]), xytext = (5,0), 
+                    textcoords='offset pixels')
+    
+    
+    plt.title('Pharmacies')
+    #plt.show()
